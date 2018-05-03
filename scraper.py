9b40ef13c02cee4ba51d7bc58615fd2c51478086
VenkatKrish17 <venkatkrishnan.btech@gmail.com>
2018-05-03 15:35:50 +0800

    linked fetched

--- scraper.py
+++ scraper.py
@@ -4,24 +4,14 @@ import requests
 import re
 from googlesearch import search
 import json
-# from nltk.corpus import stopwords
-# from nltk.stem.wordnet import WordNetLemmatizer
-# import string
-# import gensim
-# from gensim import corpora
+
 def primary_info(company_name):
-    # user_agent = 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.0.7) Gecko/2009021910 Firefox/3.0.7'
-    # headers={'User-Agent':user_agent,}
-    # url="https://www.google.com/search?q="
-    # url=url+urllib.parse.quote_plus(company_name+" wiki")
-    # #request=
     print(company_name)
     info_dict={}
     wiki_url=""
     for url in search(company_name+" company wiki", stop=1):
             wiki_url=url
             break
-
     resp=requests.get(wiki_url)
     soup=BeautifulSoup(resp.text)
     #fetching about from wikipedia intro about the company
@@ -29,7 +19,6 @@ def primary_info(company_name):
         info_dict['About']=soup.select('div.mw-parser-output > p')[0].text
     except:
         info_dict['About']='NA'
-
     info_tables=soup.find_all('table',class_='infobox')
     # print(info_tables[0].parent.select('p')[0].text)
     #if info table is available, scraping it to get the data and makeing it a dict
@@ -79,23 +68,6 @@ def primary_info(company_name):
 
 
 
-# def find_profile(about):
-#     docs=about.split(".")
-#     stop = set(stopwords.words('english'))
-#     exclude = set(string.punctuation)
-#     lemma = WordNetLemmatizer()
-#     cleaned=[]
-#     for localabout in docs:
-#         stop_free = " ".join([i for i in localabout.lower().split() if i not in stop])
-#         punc_free = ''.join(ch for ch in stop_free if ch not in exclude)
-#         normalized = " ".join(lemma.lemmatize(word) for word in punc_free.split())
-#         cleaned.append(normalized)
-#     print(normalized)
-#     dictionary=corpora.Dictionary(cleaned)
-#     doc_term_matrix=dictionary.doc2bow(docs)
-#     Lda = gensim.models.ldamodel.LdaModel# Running and Trainign LDA model on the document term matrix.
-#     ldamodel = Lda(doc_term_matrix, num_topics=3, id2word = dictionary, passes=50)
-#     print(ldamodel.print_topics(num_topics=3, num_words=3))
 
 
 def get_website(company):
@@ -116,7 +88,7 @@ def find_links(org_url):
             resp=requests.get(url)
     else:
         resp=requests.get(org_url)
-    tags=['a','li','ul','ol','div','span']
+    tags=['a','li','nav','ul','ol','div','span','footer']
     soup=BeautifulSoup(resp.text)
     #print(tags)
     menutags=[]
@@ -126,7 +98,38 @@ def find_links(org_url):
         for i in range(0,len(menutags)):
             linktags+=menutags[i].select("[href]")
             #print(linktags)
+    navtags=soup.find_all('nav')
+    for i in range(0,len(navtags)):
+        linktags+=navtags[i].select("[href]")
+    if(len(linktags)==0):
+        print("finding addtional links")
+        localtags=soup.find_all('a')
+        for i in range(0,len(localtags)):
+            linktags+=navtags[i].select("[href]")
     return(linktags)
+
+def find_and_write_links():
+    try:
+        links_array=[]
+        namesfile=open('sourcedata.txt')
+        for company in namesfile:
+            try:
+                org_url=get_website(company.strip())
+                print(org_url)
+                if(org_url!='NA'):
+                    links=find_links(org_url)
+                    comp_link_array=get_navigatable_links(links)
+                    #print(comp_link_array)
+                    links_array.append({company.strip():comp_link_array})
+                    #print(links_array)
+            except:
+                pass
+        linksfile=open("links_file.json","w",encoding="utf-8")
+        json.dump(links_array, linksfile,ensure_ascii=False)
+
+            #crawl_sub_pages(links)
+    except Exception as e:
+        print(e)
 
 def fetch_and_write():
     file=open('sourcedata.txt')
@@ -145,38 +148,109 @@ def read_from_datasource():
     data_file=open('database.json',encoding='utf-8')
     data_array=json.load(data_file)
 
-def crawl_sub_pages(links):
+
+def get_navigatable_links(links):
     links=set(links)
     hrefs=[]
     for link in links:
-        hrefs.append(link.attrs['href'])
-    return hrefs
+        hrefs.append(link.attrs['href'].lower())
+    return list(set(hrefs))
 
+def categorise_links(sub_links):
+    categorized_links={"about":[],"contact":[]}
+    for sub_link in sub_links:
+        if("about" in str(sub_link) or "profile" in str(sub_link)):
+            categorized_links['about'].append(sub_link)
+        elif("contact" in str(sub_link) or
+        "reach" in str(sub_link) or
+        "support" in str(sub_link) or
+        "enquire" in str(sub_link) or
+        "customer-service" in str(sub_link) or
+        "support" in str(sub_link) or
+        "help" in str(sub_link)):
+            categorized_links['contact'].append(sub_link)
+        else:
+            pass
+    return(categorized_links)
+
+
+comp_contact_dict={}
+def crawl_sub_pages(company_name):
+    try:
+        links_file=open('links_file.json',encoding='utf-8')
+        links_array=json.load(links_file)
+        global comp_contact_dict
+        for i in links_array:
+            #print(list(i.keys())[0])
+            if(list(i.keys())[0]==company_name):
+                #print()
+                sub_links=list(i.values())[0];
+                categorized=categorise_links(sub_links)
+            #fetching contacts
+                #print(categorized)
+                comp_contact_dict[company_name]=[]
+                for i in categorized['contact']:
+                    url=""
+                    if(not i.startswith('http')):
+                        url=get_website(company_name)+"/"+i
+                    else:
+                        url=i
+                    resp=requests.get(url)
+                    #print(str(resp.text))
+                    #print(re.findall('^[+\d]+([\s\-\(\{\[]*[\d]+[\)\]\}]*){1,10}',str(resp.text))[0])
+                    num_set=set()
+                    contacts=re.findall('([+\(]+([\s\-\(\{\[]*[\d]+[\)\]\}]*){7,10})',str(resp.text))
+                    if(len(contacts)>0):
+                        print(len(contacts))
+                        for number in contacts:
+                            num_set.add(number[0])
+                        comp_contact_dict[company_name]=set(comp_contact_dict[company_name] + list(num_set))
+                    else:
+                        print("searching in about")
+                        for in_i in categorized['about']:
+                            url=""
+                            if(not in_i.startswith('http')):
+                                url=get_website(company_name)+"/"+in_i
+                            else:
+                                url=in_i
+                            resp=requests.get(url)
+                            #print(str(resp.text))
+                            #print(re.findall('^[+\d]+([\s\-\(\{\[]*[\d]+[\)\]\}]*){1,10}',str(resp.text))[0])
+                            num_set=set()
+                            abt_contacts=re.findall('([+\(]+([\s\-\(\{\[]*[\d]+[\)\]\}]*){7,10})',str(resp.text))
+                            if(len(contacts)>0):
+                                for number in abt_contacts:
+                                    num_set.add(number[0])
+                                comp_contact_dict[company_name]=set(comp_contact_dict[company_name] + list(num_set))
+
+
+                    print(comp_contact_dict)
+
+                    #print(re.findall('([+\(\d]+([\s\-\(\{\[]*[\d]+[\)\]\}]*){7,10})',"(65) 6786 2866")[1
+
+            else:
+                pass
+            #print("No link found :(  ")
+    except:
+        pass
+
+#fetch all wiki info and official website
 #fetch_and_write()
+#read from the data source and store in a global variable
 read_from_datasource()
+#find all navigable links
+find_and_write_links()
+#crawl_sub_pages('Near')
+#crawl_sub_pages('ABR Holdings Ltd')
+#crawl_sub_pages('FilmTack')
+f=open("sourcedata.txt")
+for line in f:
+    #print(line)
+    crawl_sub_pages(line.strip())
+#crawl_sub_pages('Venture Corporation')
 # company='Far East Orchard'
 
-try:
-    links_array=[]
-    namesfile=open('sourcedata.txt')
-    for company in namesfile:
-        try:
-            org_url=get_website(company.strip())
-            print(org_url)
-            if(org_url!='NA'):
-                links=find_links(org_url)
-                comp_link_array=crawl_sub_pages(links)
-                print(comp_link_array)
-                links_array.append({company.strip():comp_link_array})
-                #print(links_array)
-        except:
-            pass
-    linksfile=open("links_file.json","w",encoding="utf-8")
-    json.dump(links_array, linksfile,ensure_ascii=False)
 
-        #crawl_sub_pages(links)
-except Exception as e:
-    print(e)
 
 # org_url=get_website(company)
 #if(org_url!='NA'):
